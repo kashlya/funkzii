d2820c8a2e1ab8fcf4fc1677c37b4579dfce81eb
38096 <05lozaverlan@gmail.com>
2024-02-01 14:06:54 +0100

    1.1

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 pervaia = int(input("первая цифра: "))
 vtoraia = int(input("вторая цифра: "))
 for i in range(pervaia, vtoraia + 1):
-    if i % 7 == 0:
-        print(i)
+    print(i, end=" ")
+print()
+for s in range(vtoraia, pervaia - 1, - 1):
+    print(s, end=" ")
+print()
+for t in range(pervaia, vtoraia + 1):
+    if t % 7 == 0:
+        print(t, end=" ")
+print()
+for w in range(pervaia, vtoraia + 1):
+    if w % 5 == 0:
+        krt_5 = 0
+        krt_5 += 1
+        print(krt_5, end=" ")
