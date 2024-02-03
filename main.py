8d51ccd4f1c931d035ae811a8345035f5c2b14d8
38096 <05lozaverlan@gmail.com>
2024-02-03 15:57:59 +0100

    2.2

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-pervoe = int(input("длина: "))
-vtoroe = input("знак: ")
-for i in range(pervoe):
-  print(vtoroe)
+pervoe = int(input("число: "))
+if pervoe == 7:
+    print("Good bye!")
+elif pervoe < 0:
+    print("Number is negative")
+elif pervoe == 0:
+    print("Number is equal to zero")
+elif pervoe > 0:
+    print("Number is positive")
