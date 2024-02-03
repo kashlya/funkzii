d3d0c4e08307f22acd2dcd7377031a6f65bc6c6f
38096 <05lozaverlan@gmail.com>
2024-02-03 15:41:15 +0100

    2.1

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-pervoe = int(input("начало: "))
-vtoroe = int(input("конец: "))
-summa = 0
-summa_2 = 0
-summa_3 = 0
-for i in range(pervoe, vtoroe + 1):
-    if i % 2 == 0:
-        summa += i
-print(f"сумма парных: {summa}")
-for s in range(pervoe, vtoroe + 1):
-    if not s % 2 == 0:
-        summa_2 += s
-print(f"сумма не парных: {summa_2}")
-for r in range(pervoe, vtoroe + 1):
-    if r % 9 == 0:
-        summa_3 += r
-print(f"сумма крат. 9: {summa_3}")
-print(f"среднеарифметическое {summa + summa_2 + summa_3}")
+pervoe = int(input("длина: "))
+vtoroe = input("знак: ")
+for i in range(pervoe):
+  print(vtoroe)
