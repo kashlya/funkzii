aed103b32d67032d4f094597c02c09eee4dafb4f
38096 <05lozaverlan@gmail.com>
2024-02-03 15:32:54 +0100

    2.0

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
-pervaia = int(input("первая цифра: "))
-vtoraia = int(input("вторая цифра: "))
-for i in range(pervaia, vtoraia + 1):
-    if i % 5 == 0 and i % 3 == 0:
-        print("Fizz Buzz")
-    elif i % 5 == 0:
-        print("Buzz")
-    elif i % 3 == 0:
-        print("Fizz")
-    else:
-        print(i)
+pervoe = int(input("начало: "))
+vtoroe = int(input("конец: "))
+summa = 0
+summa_2 = 0
+summa_3 = 0
+for i in range(pervoe, vtoroe + 1):
+    if i % 2 == 0:
+        summa += i
+print(f"сумма парных: {summa}")
+for s in range(pervoe, vtoroe + 1):
+    if not s % 2 == 0:
+        summa_2 += s
+print(f"сумма не парных: {summa_2}")
+for r in range(pervoe, vtoroe + 1):
+    if r % 9 == 0:
+        summa_3 += r
+print(f"сумма крат. 9: {summa_3}")
+print(f"среднеарифметическое {summa + summa_2 + summa_3}")
