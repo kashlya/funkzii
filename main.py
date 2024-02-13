42f65ff01f21fcf9ee7cbfe7e133ce4dd0d6486e
38096 <05lozaverlan@gmail.com>
2024-02-13 20:28:33 +0100

    1.2

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
 import random
 
-random_numbers_1 = [random.randint(0, 40) for i in range(3)]  # первый список
-print(random_numbers_1)
-random_numbers_2 = [random.randint(0, 40) for s in range(3)]  # вророй список
-print(random_numbers_2)
-
-random_numbers_3 = random_numbers_1 + random_numbers_2, set(random_numbers_1 + random_numbers_2), list(
-    set(random_numbers_1).intersection(random_numbers_2)), min(random_numbers_1 + random_numbers_2), max(
-    random_numbers_1 + random_numbers_2)
-print(random_numbers_3)
+random_numbers = [random.randint(-20, 20) for i in range(10)]
+print(random_numbers)
+min_ = random_numbers[0]
+max_ = random_numbers[0]
+for num in random_numbers:
+    if num < min_:
+        min_ = num
+    elif num > max_:
+        max_ = num
+print(min_, max_)
+minus = 0
+plus = 0
+nol = 0
+for s in random_numbers:
+    if s < min_:
+        min_ = s
+    if s > max_:
+        max_ = s
+    if s < 0:
+        minus += 1
+    elif s > 0:
+        plus += 1
+    else:
+        nol += 1
+print(f"\nmin{min_}", f"\nmax {max_}", f"\nminus {minus}", f"\nplus {plus}", f"\nnol {nol}")
