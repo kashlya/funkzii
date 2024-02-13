5cbee46e9f6d7a9705ac17913189107ef4351288
38096 <05lozaverlan@gmail.com>
2024-02-13 19:23:56 +0100

    1.

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ print(random_numbers_1)
 random_numbers_2 = [random.randint(0, 40) for s in range(3)]  # вророй список
 print(random_numbers_2)
 
-random_numbers_3 = random_numbers_1 + random_numbers_2, set(random_numbers_1 + random_numbers_2), list(set(random_numbers_1).intersection(random_numbers_2)), min(random_numbers_1 + random_numbers_2), max(random_numbers_1 + random_numbers_2)
+random_numbers_3 = random_numbers_1 + random_numbers_2, set(random_numbers_1 + random_numbers_2), list(
+    set(random_numbers_1).intersection(random_numbers_2)), min(random_numbers_1 + random_numbers_2), max(
+    random_numbers_1 + random_numbers_2)
 print(random_numbers_3)
