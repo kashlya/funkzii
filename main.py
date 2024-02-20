59afe2fe8cab241c7043c3224c2092a02bc121f5
38096 <05lozaverlan@gmail.com>
2024-02-20 18:41:32 +0100

    2

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 chislo = 6
-nomer = 1
-for i in range(chislo + 1):
-    for s in range(i):
-        print(nomer, end=" ")
-        nomer += 1
+for i in range(chislo):
+    for s in range(1, i + 1):
+        print(s, end=" ")
+    for s in range(i - 1, 0, -1):
+        print(s, end=" ")
     print()
