993077dc80e0b7a48c5e1c9c1874f775f0735cbb
38096 <05lozaverlan@gmail.com>
2024-02-20 22:24:06 +0100

    3

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
-chislo = 6
-for i in range(chislo):
-    for s in range(1, i + 1):
-        print(s, end=" ")
-    for s in range(i - 1, 0, -1):
-        print(s, end=" ")
-    print()
+a = int(input("число a: "))
+b = int(input("число b: "))
+suma = 0
+dilniki = a
+if a < b:
+    for i in range(a, b + 1):
+        ch = 0
+        for s in range(1, i + 1 // 2 + 1):
+            if i % s == 0:
+                ch += s
+        if ch > suma:
+            dilniki = i
+            suma = ch
+    print(dilniki, suma)
+else:
+    print("false")
