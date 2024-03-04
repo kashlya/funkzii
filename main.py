f62166236c68a326f3f48f76084292547e23dd6f
38096 <05lozaverlan@gmail.com>
2024-03-04 16:50:35 +0100

    funk_1

--- main.py
+++ main.py
@@ -1,16 +1,6 @@
-a = int(input("число a: "))
-b = int(input("число b: "))
-suma = 0
-dilniki = a
-if a < b:
-    for i in range(a, b + 1):
-        ch = 0
-        for s in range(1, i + 1 // 2 + 1):
-            if i % s == 0:
-                ch += s
-        if ch > suma:
-            dilniki = i
-            suma = ch
-    print(dilniki, suma)
-else:
-    print("false")
+def lin_f(l=5):
+    for i in range(1, l + 1):
+        print("*")
+
+
+lin_f()
